e5e88ad9459285b7fee5425556ab3cc5de82e3f2
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-14 18:07:47 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Artist
+  attr_accessor :name
+  attr_reader :song
+  @@all = 0
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def save
+    @@all << self
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @songs = []
+  end
+
+end
